bba09c1e19879014cdb5a4e515087e67cb95c0f5
Slayuh9 <98786500+Slayuh9@users.noreply.github.com>
2022-03-31 04:09:31 -0600

    Created Paddle Barrier
    Created paddle barrier in the octagon shape
    Can rotate the paddles with 'a' , 'd' , 'LeftArrow' , 'RightArrow'

---
+++ Orbit Pong/Assets/prototype V1/scripts/PaddleBarrier.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBarrier : MonoBehaviour
+{
+    public bool invertRotation;
+    [Tooltip ("degrees per second")]public float turnSpeed = 90;
+    public Transform paddleParent;
+
+    private void Awake() {
+        if (paddleParent == null) paddleParent = transform;
+    }
+
+    private void Update() {
+        float rotateAmount = Input.GetAxis("Horizontal");
+        rotateAmount *= turnSpeed * Time.deltaTime;
+
+        if (invertRotation) rotateAmount = -rotateAmount;
+
+        // a positive rotateAmount is counter-clockwise. negative is clockwise
+        paddleParent.transform.Rotate(Vector3.forward, rotateAmount);
+    }
+}
