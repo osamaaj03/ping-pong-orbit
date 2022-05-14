65b93a36662d57b4f698ebb1638d2c279d5e858e
Greggles <gregory@wetransfer.com>
2022-05-14 17:30:05 +0200

    Enemy base, take damage from projectile

--- Orbit Pong/Assets/Scripts/Runes/BarrageProjectileHandler.cs
+++ Orbit Pong/Assets/Scripts/Runes/BarrageProjectileHandler.cs
@@ -1,26 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pong.Entities;
+using System.Linq;
 
 public class BarrageProjectileHandler : ProjectileHandler
 {
 
     Vector3 velocity;
 
-    private void Awake() {
+    [SerializeField] float destroyAfterTime = 3f;
+    float timeAlive = 0;
+
+    private void Awake()
+    {
         velocity = Random.insideUnitCircle * 15;
 
-        // try to find a target
-        // for now, just get the ball
-        target = GameObject.Find("Ball").transform;
+        List<Enemy> enemies = FindObjectsOfType<Enemy>().ToList();
+
+        if (enemies.Count > 0)
+        {
+            // Find transforms of all enemies and store in a list
+            List<Transform> enemyTransforms = new List<Transform>();
+            foreach (Enemy enemy in enemies)
+            {
+                enemyTransforms.Add(enemy.transform);
+            }
+
+            // Find the closest enemy to the projectile
+            Transform closestEnemy = enemyTransforms.OrderBy(x => Vector3.Distance(transform.position, x.position)).First();
+            target = closestEnemy;
+        }
     }
 
-    private void Update() {
+    private void OnDisable()
+    {
+        timeAlive = 0;
+    }
+
+    private void Update()
+    {
+        timeAlive += Time.deltaTime;
+        if (timeAlive >= destroyAfterTime) { gameObject.SetActive(false); }
+
         // move towards a target
         // if no target can be found, just go to the world origin
         Vector3 targetPos = Vector3.zero;
 
-        if (target != null) {
+        if (target != null)
+        {
             targetPos = target.position;
         }
 
@@ -29,14 +57,5 @@ public class BarrageProjectileHandler : ProjectileHandler
         velocity = Vector3.MoveTowards(velocity, targetVel, 40 * Time.deltaTime);
 
         transform.position += velocity * Time.deltaTime;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        // if other can be damaged, deal damage
-        // and disable or destroy the projectile
-
-        if (other.CompareTag("Ball")) {
-            Destroy(gameObject);
-        }
     }
 }
